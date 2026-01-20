0841e3a3076801ad6e1222c96722da3287c588d0
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-19 19:27:03 -0800

    First playable version

--- src/main.py
+++ src/main.py
@@ -7,6 +7,10 @@ question_bank = []
 for question in question_data:
     question_bank.append(Question(question["text"], question["answer"]))
 
-question = QuizBrain(question_bank)
+quiz = QuizBrain(question_bank)
 
-question.next_question()
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"You final score was: {quiz.score}/{quiz.question_number}")
