e5933741f51a83f0572373079054f233f4f34730
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-19 21:12:26 -0800

    Add new bank of data

--- src/data.py
+++ src/data.py
@@ -1,14 +1,221 @@
+# question_data = [
+# {"text": "A slug's blood is green.", "answer": "True"},
+# {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+# {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+# {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+# {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+# {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+# {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+# {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+# {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+# {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+# {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+# {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+
+
+# question_data = [
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "The Windows 7 operating system has six main editions.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "Linus Torvalds created Linux and Git.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "In most programming languages, the operator ++ is equivalent to the statement '+='.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "Linux was first created as an alternative to Windows XP.",
+#       "correct_answer": "False",
+#       "incorrect_answers": [
+#         "True"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "The first IBM PC was released in 1981.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "A Mac is not a PC",
+#       "correct_answer": "False",
+#       "incorrect_answers": [
+#         "True"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "RAM stands for Random Access Memory.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+#       "correct_answer": "False",
+#       "incorrect_answers": [
+#         "True"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "Time on Computers is measured via the EPOX System.",
+#       "correct_answer": "False",
+#       "incorrect_answers": [
+#         "True"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "easy",
+#       "category": "Science: Computers",
+#       "question": "The logo for Snapchat is a Bell.",
+#       "correct_answer": "False",
+#       "incorrect_answers": [
+#         "True"
+#       ]
+#     }
+#   ]
+
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "General Knowledge",
+      "question": "The commercial UK channel ITV stands for 'International Television'.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "General Knowledge",
+      "question": "Sitting for more than three hours a day can cut two years off a person&#039;s life expectancy.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "General Knowledge",
+      "question": "'27 Club' is a term used to refer to a list of famous actors, musicians, and artists who died at the age of 27.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "General Knowledge",
+      "question": "SCP-173 was the first SCP article written for the web-based collaborative fiction project known as the 'SCP Foundation'",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "General Knowledge",
+      "question": "Popcorn was invented in 1871 by Frederick W. Rueckheim in the USA where he sold the snack on the streets of Chicago.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "General Knowledge",
+      "question": "Albert Einstein had trouble with mathematics when he was in school.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "General Knowledge",
+      "question": "Pure water effectively conducts electricity ",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "General Knowledge",
+      "question": "A pencil&#039;s lead is typically made from graphite, not lead",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "General Knowledge",
+      "question": "March 10th is also known as Mar10 Day.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "General Knowledge",
+      "question": "Pluto is a planet.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    }
+  ]
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    question_bank.append(Question(question["text"], question["answer"]))
+    question_bank.append(Question(question["question"], question["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
 
@@ -13,4 +13,4 @@ while quiz.still_has_questions():
     quiz.next_question()
 
 print("You've completed the quiz")
-print(f"You final score was: {quiz.score}/{quiz.question_number}")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
